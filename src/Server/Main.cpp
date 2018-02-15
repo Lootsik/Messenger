4ad5d10a055761f5d44be973b1dcd84eb4352143
lootsik1 <lootsik1@gmail.com>
2018-02-15 21:59:38 +0200

    close on ctrl c

--- src/Server/Main.cpp
+++ src/Server/Main.cpp
@@ -1,9 +1,24 @@
 #include <Network/Network.h>
 #include <Database/Database.h>
 #include <Logger/Logger.h>
-
+#include <Windows.h>
 const char* ConfigFilename = "MessengerServer.cfg";
 const char* LogFilename = "Network.log";
+
+boost::asio::io_service service;
+
+
+BOOL WINAPI consoleHandler(DWORD signal) 
+{
+	if (signal == CTRL_C_EVENT)
+	{
+		boost::system::error_code err;
+		service.stop();
+		Logger::Log(Logger::Success, "Server stopped");
+	}
+	return TRUE;
+}
+
 
 int main()
 {
@@ -17,7 +32,6 @@ int main()
 		return 1;
 	}
 	
-	boost::asio::io_service service;
 	Network server{ service };
 
 	if (!server.LoadFromConfig(ConfigFilename))
@@ -35,6 +49,11 @@ int main()
 	server.SetMessageEngine(&engine);
 	if (!server.Start())
 		return 1;
-		
+	
+	if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
+		Logger::Log(Logger::Error, "Could not set control handler");
+		return 2;
+	}
+
 	service.run();
 }
